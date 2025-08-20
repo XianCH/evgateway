7692d1f998ab1039a50646a645a27699b6c3fdf3
XianCH <x14n911@gmail.com>
2025-08-20 22:58:46 +0800

    feat: add more errors

--- internal/protocol/errors.go
+++ internal/protocol/errors.go
@@ -4,4 +4,8 @@ import "errors"
 
 var (
 	PayloadTooLargeError = errors.New("payload size exceeds maximum allowed size")
+
+	ErrNeedMoreData = errors.New("need more data to parse frame")
+
+	ErrCRCMismatch = errors.New("crc mismatch")
 )
